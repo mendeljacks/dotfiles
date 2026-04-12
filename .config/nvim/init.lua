5346a838ad3cf89e5209c1d690ebf3cbed2e1a60
Mendel <mendeljacks@gmail.com>
2026-04-12 01:19:51 -0400

    fix vim fugitive in dotfiles repo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,9 @@
+-- Set git dir and worktree location if we are in home directory so vim fugitive works
+if vim.fn.getcwd() == vim.fn.expand("~") then
+  vim.env.GIT_DIR = vim.fn.expand("~/.dotfiles")
+  vim.env.GIT_WORK_TREE = vim.fn.expand("~")
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
