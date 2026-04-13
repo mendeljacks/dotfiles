69c73a8b82a5e480881b6e32d2f9bb1331417cef
Mendel <mendeljacks@gmail.com>
2026-04-13 14:50:02 -0400

    add gs

---
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    opts = {
+      mappings = {
+        add = "gsa", -- add surround: gsa)
+        delete = "gsd", -- delete surround: gsd)
+        find = "gsf", -- find surround (to right)
+        find_left = "gsF", -- find surround (to left)
+        highlight = "gsh", -- highlight surround
+        replace = "gsr", -- replace surround: gsr)]
+        update_n_lines = "gsn", -- update `n_lines`
+      },
+    },
+  },
+}
